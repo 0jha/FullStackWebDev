c2c8c4e58e9c186c5aaed12305bd98e797ec3ffc
Kishore Kumar Ojha <kishorekumarojha9@gmail.com>
2022-09-03 18:55:06 +0400

    operator

---
+++ Operator/operator.js
@@ -0,0 +1,32 @@
+a=4;
+
+console.log(a);
+
+a=a+3;
+
+console.log(a);
+
+a +=3   // it means a = a + 3
+
+console.log(a);
+
+a+= 5 // means a= a + 5
+
+console.log(a);
+
+a+=5
+
+console.log(a);
+
+a-+5
+
+console.log(a);
+
+a*=5
+
+console.log(a);
+
+a/=5
+
+console.log(a);
+
