3179ad11bbbae48c892ed06091c30bf0cdd5e1f2
Kishore Kumar Ojha <kishorekumarojha9@gmail.com>
2022-09-07 20:42:07 +0400

    JSON Chapter

---
+++ Javascript Object and JSON/JS Object.js
@@ -0,0 +1,40 @@
+// object
+//const student = {
+    //firstName: 'ram',
+    //class: 10
+//};
+
+//const object_name = {
+   // key1: value1,
+   // key2: value2
+ //}
+
+ // object creation
+//const person = {
+   // name: 'John',
+   // age: 20
+//};
+//console.log(typeof person); // object
+
+//const person1 = { name: 'John', age: 20 };
+
+const sampleObj=
+{
+    key1: "Value1",
+    key2: 22,
+    key3: function(){
+        console.log("function")
+    }
+}
+
+const val = sampleObj.key1;
+
+const val1 = sampleObj["key1"];
+
+console.log(val);
+console.log(val1);
+
+
+
+
+
